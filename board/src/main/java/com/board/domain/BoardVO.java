68136f1b0c67466cfc99f84d7d0f150e664d680b
imjeong-gyu <imjeong-gyu@localhost>
2023-03-08 16:22:44 +0900

    DB 연동, 게시물 리스트 확인 뷰,컨트롤러,서비스 생성

---
+++ board/src/main/java/com/board/domain/BoardVO.java
@@ -0,0 +1,49 @@
+package com.board.domain;
+
+import java.util.Date;
+
+public class BoardVO {
+	
+	private int board_id;
+	private String title;
+	private String content;
+	private String writer;
+	private Date regDate;
+	public int getBoard_id() {
+		return board_id;
+	}
+	public void setBoard_id(int board_id) {
+		this.board_id = board_id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getViewCount() {
+		return viewCount;
+	}
+	public void setViewCount(int viewCount) {
+		this.viewCount = viewCount;
+	}
+	private int viewCount;
+}
